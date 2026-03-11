ee4992fdc461916295b63378a55a9f8264b0958f
aryan2621 <aryanverma2412@gmail.com>
2026-03-11 18:54:33 +0530

    refactor: remove unused environment variable constants and clean up exports in index file

--- app-market-place-admin/client/lib/constants/index.ts
+++ app-market-place-admin/client/lib/constants/index.ts
@@ -1,5 +1,4 @@
 export { ContentType, HttpMethod } from "./http";
-export { EnvVar } from "./env";
 export { DEFAULT_BASE_URL } from "./defaults";
 export { EMPTY_STRING, ErrorMessages } from "./errors";
 export { PLATFORM_LABEL_LONG, PLATFORM_LABEL_SHORT, Platform, PLATFORM_VALUES } from "./platform";
